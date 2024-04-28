4697a236582ce4f0c8c78db196d9098e518bf2f4
Anil Pal <anilcodes01@gmail.com>
2024-04-28 23:03:30 +0530

    backend dev

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('Hello world!')
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello world!')
+})
+
+app.listen(3000, (req, res) => {
+    console.log('Server app listening on port 3000...')
+})
